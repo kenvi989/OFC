a2621bc653fb91623985225166292802016a4ca6
kenvi989 <109719003+kenvi989@users.noreply.github.com>
2022-11-11 10:49:25 +0900

    Add files via upload

---
+++ OFCxlsReader/OFCxlsReader/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace OFCxlsReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboSheet = new System.Windows.Forms.ComboBox();
+            this.txtFilename = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbTemplate = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(730, 332);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(58, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 17);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 243);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 332);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File name:";
+            // 
+            // cboSheet
+            // 
+            this.cboSheet.FormattingEnabled = true;
+            this.cboSheet.Location = new System.Drawing.Point(92, 368);
+            this.cboSheet.Name = "cboSheet";
+            this.cboSheet.Size = new System.Drawing.Size(121, 23);
+            this.cboSheet.TabIndex = 3;
+            this.cboSheet.SelectedIndexChanged += new System.EventHandler(this.cboSheet_SelectedIndexChanged);
+            // 
+            // txtFilename
+            // 
+            this.txtFilename.Location = new System.Drawing.Point(92, 332);
+            this.txtFilename.Name = "txtFilename";
+            this.txtFilename.ReadOnly = true;
+            this.txtFilename.Size = new System.Drawing.Size(632, 22);
+            this.txtFilename.TabIndex = 4;
+            this.txtFilename.TextChanged += new System.EventHandler(this.txtFilename_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 371);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Sheet:";
+            // 
+            // cmbTemplate
+            // 
+            this.cmbTemplate.FormattingEnabled = true;
+            this.cmbTemplate.Location = new System.Drawing.Point(92, 286);
+            this.cmbTemplate.Name = "cmbTemplate";
+            this.cmbTemplate.Size = new System.Drawing.Size(632, 23);
+            this.cmbTemplate.TabIndex = 6;
+            this.cmbTemplate.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 289);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Template:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(300, 435);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 44);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "実行";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 532);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbTemplate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFilename);
+            this.Controls.Add(this.cboSheet);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnBrowse);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Read *.xls or *.xlsx";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboSheet;
+        private System.Windows.Forms.TextBox txtFilename;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbTemplate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ OFCxlsReader/OFCxlsReader/Form1.cs
@@ -0,0 +1,282 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using InDesign;
+
+namespace OFCxlsReader
+{
+    public partial class Form1 : Form
+    {
+        public InDesign.Application _application = null;
+        public Document _docmument;
+        public Form1()
+        {
+            InitializeComponent();
+            LoadTemplate();
+        }
+
+        DataTableCollection tableCollection;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog openFileDialog=new OpenFileDialog() { Filter="Excel 97-2003 Workbook|*.xls|Excel Workbook|*.xlsx" })
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtFilename.Text = openFileDialog.FileName;
+                    using(var stream = File.Open(openFileDialog.FileName, FileMode.Open, FileAccess.Read))
+                    {
+                        using(IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream))
+                        {
+                            DataSet result = reader.AsDataSet(new ExcelDataSetConfiguration()
+                            {
+                                ConfigureDataTable=(_)=>new ExcelDataTableConfiguration() { UseHeaderRow = true}
+                            });
+                            tableCollection = result.Tables;
+                            cboSheet.Items.Clear();
+                            foreach (DataTable table in tableCollection)
+                                cboSheet.Items.Add(table.TableName);//add sheet to combobox
+                        }
+                    }
+                }
+            }
+        }
+        //private void GetTables(DataSet dataSet)
+        //{
+        //    // Get Each DataTable in the DataTableCollection and
+        //    // print each row value.
+        //    foreach (DataTable table in dataSet.Tables)
+        //        foreach (DataRow row in table.Rows)
+        //            foreach (DataColumn column in table.Columns)
+        //                if (row[column] != null)
+        //                    Console.WriteLine(row[column]);
+        //}
+
+        private void CreateTable(DataSet dataSet)
+        {
+            DataTable newTable = new DataTable("table");
+            newTable.Columns.Add("出発都市3コード", typeof(string));
+            newTable.Columns.Add("出発都市3コード");
+            dataSet.Tables.Add(newTable);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboSheet_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataTable dt = tableCollection[cboSheet.SelectedItem.ToString()];
+            dataGridView1.DataSource = dt;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtFilename_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbTemplate_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool DoScripts(string jsxFile)
+        {
+            try
+            {
+                string fileName = System.IO.Path.GetFileNameWithoutExtension(jsxFile);
+                _application.DoScript(jsxFile, idScriptLanguage.idJavascript);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("ErrorExecuteJSX　エラー:　" + ex.Message, "通知", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                MessageBox.Show("テンプレート、または紙面体裁テキストファイル名等のご確認をお願い致します。");
+                return false;
+            }
+        }
+
+        private void LoadTemplate()
+        {
+            try
+            {
+                List<string> TemplateList = new List<string>();
+                string folder = System.IO.Path.Combine(System.Windows.Forms.Application.StartupPath);
+                if (Directory.Exists(folder + "\\Template"))
+                    folder = folder + "\\Template";
+                else if (Directory.Exists(folder + "\\template"))
+                    folder = folder + "\\template";
+                string[] tempFiles = Directory.GetFiles(folder, "*.indd");
+                foreach (string templates in Directory.GetFiles(folder, "*.indd"))
+                { TemplateList.Add(templates); }
+                foreach (string items in TemplateList)
+                {
+                    cmbTemplate.Items.Add(System.IO.Path.GetFileName(items));
+                }
+            }
+            catch (Exception exp) { }
+        }
+
+        private Document OpenDocument(string filePath)
+        {
+            Document documentTemplate = null;
+            string fileName = System.IO.Path.GetFileName(filePath);
+
+            //Open document
+            if (documentTemplate == null)
+            {
+                documentTemplate = _application.Open(filePath, false);
+            }
+            // Global variable setting
+
+            this._docmument = documentTemplate;
+            return documentTemplate;
+        }
+        private InDesign.Application OpenApplication()
+        {
+            try
+            {
+                Type type2018 = Type.GetTypeFromProgID("InDesign.Application.CC.2018");
+                Type type2019 = Type.GetTypeFromProgID("InDesign.Application.CC.2019");
+                Type type = Type.GetTypeFromProgID("InDesign.Application");
+                if (type2018 == null & type2019 == null & type == null)
+                {
+                    throw new Exception("Adobe InDesign CC is not installed");
+                    return null;
+                }
+                else if (type2018 != null) _application = (InDesign.Application)Activator.CreateInstance(type2018);
+                else if (type2019 != null) _application = (InDesign.Application)Activator.CreateInstance(type2019);
+                else _application = (InDesign.Application)Activator.CreateInstance(type);
+                //app = COMCreateObject("InDesign.Application");
+                if (_application == null)
+                {
+                    if (type2018 != null) _application = (InDesign.Application)Activator.CreateInstance(type2018);
+                    else if (type2019 != null) _application = (InDesign.Application)Activator.CreateInstance(type2019);
+                    else _application = (InDesign.Application)Activator.CreateInstance(type);
+                    System.Threading.Thread.Sleep(10000);
+                }
+                // app.scriptPreferences.userInteractionLevel = UserInteractionLevels.NEVER_INTERACT;               
+                return _application;
+            }
+            catch (Exception ex)
+            {
+                //log.Error(ex);
+                return null;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            // Create a StringBuilder that expects to hold 50 characters.
+            StringBuilder sb = new StringBuilder();
+
+            //string bfShuppatsu = "";
+            string bfChaku = "";
+            string bfKaisha = "";
+
+
+            List<string[]> lstJuryo = new List<string[]>();
+            bool findJuryo = false;
+            List<string> lstKaisha = new List<string>();
+            bool findKaisha = false;
+
+            DataTable table = tableCollection[cboSheet.SelectedItem.ToString()];
+            foreach (DataRow row in table.Rows)
+            {
+
+                if (bfChaku == row["到着都市3コード"].ToString())
+                {
+                    //adilhan hotiin medeelliin davhtsaliig shalgaj bgaa heseg
+                    if (bfKaisha != row["航空会社コード"].ToString())
+                    {
+                        lstKaisha.Add(row["航空会社コード"].ToString());
+                    }
+
+                    findJuryo = false;
+                    foreach (string[] ju in lstJuryo)
+                    {
+                        if (ju[0] == row["重量"].ToString())
+                        {
+                            findJuryo = true;
+                        }
+                    }
+
+                    if (findJuryo != true)
+                    {
+                        string[] ryoandprice = { row["重量"].ToString() , row["賃率"].ToString() };
+                        lstJuryo.Add(ryoandprice);
+                    }
+                }
+                else
+                {
+                    //group bolgoson hotiin medeeleliig hevleh heseg
+                    string strkaisha = " ";
+                    foreach (string kai in lstKaisha)
+                    {
+                        if(strkaisha == " ")
+                        {
+                            strkaisha = strkaisha + kai;
+                        }
+                        else
+                        {
+                            strkaisha = strkaisha + "," + kai;
+                        }
+                    }
+
+                    findKaisha = false;
+                    foreach (string[] ju in lstJuryo)
+                    {
+                        if (findKaisha == false)
+                        {
+                            sb.AppendLine(strkaisha + "                 " + ju[0] + "      " + ju[1] + " | ");
+                            findKaisha = true;
+                        }
+                        else
+                        {
+                            sb.AppendLine("                 " + ju[0] + "      " + ju[1] + " | ");
+                        }
+                    }
+
+                    //shine hotiin medeelel avch ehelj bgaa heseg
+                    lstJuryo = new List<string[]>();
+                    lstKaisha = new List<string>();
+                    string[] ryoandprice = {row["重量"].ToString(), row["賃率"].ToString()};
+                    lstJuryo.Add(ryoandprice);
+                    lstKaisha.Add(row["航空会社コード"].ToString());
+
+                    //shine hot bolon ulsiin ner hevelj bgaa heseg
+                    sb.AppendLine(row["到着都市"].ToString() + " (" + row["到着都市3コード"].ToString() + ")      " + row["到着国"].ToString() + " | ");
+
+                }
+
+                bfChaku = row["到着都市3コード"].ToString();
+                bfKaisha = row["航空会社コード"].ToString();
+            }
+        
+
+            using (StreamWriter file = new StreamWriter(@"D:\Bold\Projects\myofc\hereIam.txt"))
+            {
+                file.WriteLine(sb.ToString()); // "sb" is the StringBuilder
+            }
+        }
+    }
+}
